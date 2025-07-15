aca11843512219a71358910b9342d729b2838edd
RedOni <RedOni@DESKTOP-70B9EN0>
2025-07-14 20:34:31 -0500

    Agregar archivos de proyecto.

---
+++ Base/BaseDeDatos.cs
@@ -0,0 +1,31 @@
+﻿using BaseDatos.Clases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseDatos.BaseDatos
+{
+    public static class BaseDeDatos
+    {
+        public static List<Cliente> BaseDatosCliente = new List<Cliente>();
+
+        public static void ImprimirClientes()
+        {
+            foreach (Cliente cliente in BaseDatosCliente)
+            {
+                cliente.imprimir();
+            }
+        }
+
+
+        public static Cliente BuscarClientePorCedula(string parametro_cedula)
+        {
+            return BaseDatosCliente.FirstOrDefault(bdc => bdc.cedula == parametro_cedula);
+        }
+
+
+
+    }
+}
